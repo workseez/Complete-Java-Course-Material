e697d5bc011e6f1c7cd90d97d7ef0ee5efe4c6ff
berakrishnendu36 <bera.krishnendu36@gmail.com>
2024-06-25 21:38:42 +0530

    Updated Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
-import com.tutorialseu.basics.LibraryManagement;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
-        //Uncomment this to run the LibraryManagement class from jar file/Main class
-//        String[] classArgs = {};
-//        LibraryManagement.main(classArgs);
     }
 }
